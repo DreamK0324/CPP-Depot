db435d8b9c04ed80c5f4a387cf7d15851a028cb9
Dream K <dreamk@ZoneBook.local>
2022-10-09 00:01:42 -0400

    clear

--- HPP-CPP-Template/main.cpp
+++ HPP-CPP-Template/main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include "Blackhole.hpp"
 #include "ArrayBag.hpp"
-#include "Star.hpp"
 
 
 int main(){
-    Star star1;
-    Blackhole<Star> hole1;
-    ArrayBag<Star> bag1;
+    ArrayBag<int> bag1;
 
 
 }
